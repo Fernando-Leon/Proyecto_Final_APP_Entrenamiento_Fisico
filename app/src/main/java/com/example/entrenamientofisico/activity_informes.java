97d4f47363712ae7d62f7f7421b35df5892bc0a5
Fernando-Leon <96993869+Fernando-Leon@users.noreply.github.com>
2022-05-08 18:42:11 -0500

    Creacion del diseño base da la activity informes asi mismo Creacion del Popup para cambiar el imc -sin diseño ni parte logica del popup

--- app/src/main/java/com/example/entrenamientofisico/activity_informes.java
+++ app/src/main/java/com/example/entrenamientofisico/activity_informes.java
@@ -5,13 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class activity_informes extends AppCompatActivity {
+
+    //Bibliografias
+
+    //https://www.youtube.com/watch?v=ARezg1D9Zd0
+    //https://www.develou.com/como-crear-dialogos-en-android/#Crear_un_dialogo_personalizado
+
+    private TextView peso;
+    private TextView altura;
+    private TextView imc;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informes);
+
+        peso = (TextView) findViewById(R.id.valuePeso);
+        altura = (TextView) findViewById(R.id.valueAltura);
+        imc = (TextView) findViewById(R.id.resultadoImc);
+        button = (Button) findViewById(R.id.abrirPopup);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+
+            private void openDialog(){
+                popupChangeImc pop = new popupChangeImc();
+                pop.show(getSupportFragmentManager(), "Change Imc");
+            }
+        });
+
+
+        String val1 = altura.getText().toString();
+        String val2 = peso.getText().toString();
+        float alt = Float.parseFloat(val1);
+        float pes = Float.parseFloat(val2);
+        float resultado = pes / (alt * alt);
+        String x = String.valueOf(resultado);
+        imc.setText(x);
     }
 
     public void statusAjustes(View view) {
@@ -27,5 +64,9 @@ public class activity_informes extends AppCompatActivity {
     public void statusInicio(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void abrirPopup(View view) {
+
     }
 }
---
+++ app/src/main/java/com/example/entrenamientofisico/popupChangeImc.java
@@ -0,0 +1,44 @@
+package com.example.entrenamientofisico;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class popupChangeImc extends AppCompatDialogFragment {
+    private EditText editAltura;
+    private EditText editPeso;
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_change_imc, null);
+        builder.setView(view)
+                .setTitle("IMC")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        editAltura = view.findViewById(R.id.editAlturaText);
+        editPeso = view.findViewById(R.id.editPesoText);
+        return builder.create();
+    }
+}
